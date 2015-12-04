17774b63bd3029113493c43bde6e231a8af05585
Hiyowa Kyobashi <kh.iyowa@gmail.com>
2015-12-05 08:25:41 +0900

    キャンバス制御のファイルを分割

---
+++ Drawing/Canvas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing
+{
+    public partial class DrawingFm
+    {
+        //  新規作成
+        private void newCanvas()
+        {
+            //  リストを初期化
+            shapeList = new List<Shape>();
+            //  デフォルトの図形と色を設定
+            currentShape = Shape.RECT;
+            currentColor = Color.Blue;
+            shapeFilled = true;
+            lineWidth = 2;
+
+            //  保存状態をクリア
+            savedState = true;
+            //  ファイル名をクリア
+            fileName = null;
+
+            //  再描画
+            this.Invalidate();
+            setCaption();
+        }
+
+        //  ステータスバーにキャンバスサイズを表示する
+        private void indicateCanvasSize()
+        {
+            Size canvasSize = this.ClientSize;
+            canvasXTssl.Text = "X = " + canvasSize.Width.ToString();
+            canvasYTssl.Text = "Y = " + (canvasSize.Height - commandBarMs.Height).ToString();
+        }
+    }
+}
--- Drawing/Drawing.cs
+++ Drawing/Drawing.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace Drawing
@@ -105,35 +104,6 @@ namespace Drawing
             {
                 sh.Draw(g);
             }
-        }
-
-        //  新規作成
-        private void newCanvas()
-        {
-            //  リストを初期化
-            shapeList = new List<Shape>();
-            //  デフォルトの図形と色を設定
-            currentShape = Shape.RECT;
-            currentColor = Color.Blue;
-            shapeFilled = true;
-            lineWidth = 2;
-
-            //  保存状態をクリア
-            savedState = true;
-            //  ファイル名をクリア
-            fileName = null;
-
-            //  再描画
-            this.Invalidate();
-            setCaption();
-        }
-
-        //  ステータスバーにキャンバスサイズを表示する
-        private void indicateCanvasSize()
-        {
-            Size canvasSize = this.ClientSize;
-            canvasXTssl.Text = "X = " + canvasSize.Width.ToString();
-            canvasYTssl.Text = "Y = " + (canvasSize.Height - commandBarMs.Height).ToString();
         }
     }
 }
