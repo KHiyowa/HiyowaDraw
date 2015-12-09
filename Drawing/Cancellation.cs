e73ff267162bbeafcb0ba9f970d748f982bd9aa9
Hiyowa Kyobashi <kh.iyowa@gmail.com>
2015-12-10 00:56:38 +0900

    キャンセル関連のファイルを分離

---
+++ Drawing/Cancellation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing
+{
+    public partial class DrawingFm
+    {
+        //  元に戻す/やり直しのスタック
+        public static Stack<Cancellation> undoStack;
+        public static Stack<Cancellation> redoStack;
+
+        abstract private class EditType
+        {
+            public static int DRAW = 0;
+            public static int ERASE = 1;
+            public static int MODIFY = 2;
+        }
+
+        public class Cancellation
+        {
+            public static int edittype;
+            public static int position;
+            public static Shape shape;
+            public int getEditType()
+            {
+                return edittype;
+            }
+            public void setEditType(int type)
+            {
+                edittype = type;
+            }
+            public int getPosition()
+            {
+                return position;
+            }
+            public void setPosition(int pos)
+            {
+                position = pos;
+            }
+            public Shape getShape()
+            {
+                return shape;
+            }
+            public void setShape(Shape sh)
+            {
+                shape = sh;
+            }
+        }
+        #region UndoRedo
+        //  元に戻す
+        public void undo()
+        {
+            cancellation(undoStack, redoStack);
+        }
+
+        //  やり直し
+        public void redo()
+        {
+            cancellation(redoStack, undoStack);
+        }
+
+        public void cancellation(Stack<Cancellation> undo, Stack<Cancellation> redo)
+        {
+            if (undo.Count == 0) { return; }
+            Cancellation cs = undo.Pop();
+            if (cs.getEditType() == EditType.DRAW)
+            {
+                shapeList.RemoveAt(cs.getPosition());
+                // cs.setEditType(EditType.ERASE);
+                redo.Push(cs);
+            }
+            else if (cs.getEditType() == EditType.ERASE)
+            {
+                shapeList.Insert(cs.getPosition(), cs.getShape());
+                // cs.setEditType(EditType.DRAW);
+                redo.Push(cs);
+            }
+            this.Invalidate();
+        }
+        #endregion
+    }
+}
--- Drawing/Canvas.cs
+++ Drawing/Canvas.cs
@@ -10,48 +10,6 @@ namespace Drawing
 {
     public partial class DrawingFm
     {
-        //  元に戻す/やり直しのスタック
-        public static Stack<Cancellation> undoStack;
-        public static Stack<Cancellation> redoStack;
-
-        abstract private class EditType
-        {
-            public static int DRAW = 0;
-            public static int ERASE = 1;
-            public static int MODIFY = 2;
-        }
-
-        public class Cancellation
-        {
-            public static int edittype;
-            public static int position;
-            public static Shape shape;
-            public int getEditType()
-            {
-                return edittype;
-            }
-            public void setEditType(int type)
-            {
-                edittype = type;
-            }
-            public int getPosition()
-            {
-                return position;
-            }
-            public void setPosition(int pos)
-            {
-                position = pos;
-            }
-            public Shape getShape()
-            {
-                return shape;
-            }
-            public void setShape(Shape sh)
-            {
-                shape = sh;
-            }
-        }
-
         //  新規作成
         private void newCanvas()
         {
@@ -84,38 +42,5 @@ namespace Drawing
             canvasXTssl.Text = "X = " + canvasSize.Width.ToString();
             canvasYTssl.Text = "Y = " + canvasSize.Height.ToString();
         }
-
-        #region UndoRedo
-        //  元に戻す
-        public void undo()
-        {
-            cancellation(undoStack, redoStack);
-        }
-
-        //  やり直し
-        public void redo()
-        {
-            cancellation(redoStack, undoStack);
-        }
-
-        public void cancellation(Stack<Cancellation> undo, Stack<Cancellation> redo)
-        {
-            if (undo.Count == 0) { return; }
-            Cancellation sh = undo.Pop();
-            if (sh.getEditType() == EditType.DRAW)
-            {
-                shapeList.RemoveAt(sh.getPosition());
-                sh.setEditType(EditType.ERASE);
-                redo.Push(sh);
-            }
-            else if (sh.getEditType() == EditType.ERASE)
-            {
-                shapeList.Insert(sh.getPosition(), sh.getShape());
-                sh.setEditType(EditType.DRAW);
-                redo.Push(sh);
-            }
-            this.Invalidate();
-        }
-        #endregion
     }
 }
